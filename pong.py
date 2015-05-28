698902d5ea41b36ca548dafba981715fc6894215
Raymond Lesley <github.raymond@thelesleys.co.uk>
2015-05-28 19:57:57 +0100

    code snapshot

---
+++ pong.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# PiPong - Pong for the Pi
+# see www.linuxuser.co.uk/tutorials/make-a-game-on-raspberry-pi
+
+import pygame
+import sys
+import random
+
+from Pygame.locals import *
+
+class PiPong
+
+if __name__ == '__main__':
+	main()
